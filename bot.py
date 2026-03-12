08a8b30f633c3bc060a3660f603c4ffb8c30e9e3
Nexus-Lisa <lisa@nexus.prime>
2026-03-12 13:24:30 +0600

    Update bot with correct token and simplified structure

--- bot.py
+++ bot.py
@@ -1,39 +1,25 @@
 from telegram import Update, KeyboardButton, ReplyKeyboardMarkup, WebAppInfo
-from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
-import json
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = "YOUR_TOKEN"
+TOKEN = "8589702180:AAFJrdZ_98E_qA4enNgmQ58Zqfs6HqxOg7Q"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     button = KeyboardButton(
-        text="Открыть приложение",
+        text="Открыть Mini App",
         web_app=WebAppInfo(url="https://github.com/Nexus-Lisa/Telegram-Mini-App")
     )
 
     keyboard = ReplyKeyboardMarkup([[button]], resize_keyboard=True)
 
     await update.message.reply_text(
-        "Откройте мини приложение",
+        "Откройте приложение",
         reply_markup=keyboard
-    )
-
-
-async def webapp(update: Update, context: ContextTypes.DEFAULT_TYPE):
-
-    data = json.loads(update.message.web_app_data.data)
-
-    name = data["name"]
-    text = data["text"]
-
-    await update.message.reply_text(
-        f"Новая заявка\n\nИмя: {name}\nСообщение: {text}"
     )
 
 
 app = ApplicationBuilder().token(TOKEN).build()
 
 app.add_handler(CommandHandler("start", start))
-app.add_handler(MessageHandler(filters.StatusUpdate.WEB_APP_DATA, webapp))
 
 app.run_polling()
